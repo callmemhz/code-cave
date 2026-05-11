b95441a27efc22bd1ecab3ad7e2ccbf6d76ade6b
callmemhz <callmemhz@gmail.com>
2026-05-11 16:34:23 +0800

    feat(nodes): terminal node with xterm + persistent scrollback

--- src/canvas/nodeTypes.ts
+++ src/canvas/nodeTypes.ts
@@ -1,6 +1,8 @@
 import type { NodeTypes } from "@xyflow/react";
 import { NoteNode } from "../nodes/NoteNode";
+import { TerminalNode } from "../nodes/TerminalNode";
 
 export const nodeTypes: NodeTypes = {
   note: NoteNode,
+  terminal: TerminalNode,
 };
